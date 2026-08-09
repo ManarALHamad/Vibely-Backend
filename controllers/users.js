840bc572b0fa5928428e13a6f8cb9adc62220393
ManarALHamad <manaralhamad2004@gmail.com>
2026-08-09 23:47:45 +0300

    profile controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const Post = require('../models/post')
 
 
 const index = async (req, res) => {
@@ -10,8 +11,31 @@ const index = async (req, res) => {
 
 }
 
+//for the user profile
+
+const profile = async (req, res) => {
+    try {
+
+        const user = await User.findById(req.user._id).select('-password')
+
+        const posts = await Post.find({
+            author: req.user._id
+        }).sort({ createdAt: -1 })
+
+        res.status(200).json({
+            user,
+            posts
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
 
 module.exports = {
 
     index,
+    profile,
 }
